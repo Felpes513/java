cb10e4e26aed6c1810f6db144d2d0576192d6dfb
Felipe Souza Moreira <110871291+Felpes513@users.noreply.github.com>
2024-04-08 15:48:54 -0300

    Add files via upload

---
+++ DataProcessing/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int x;
+        double y;
+
+        x = 5;
+        y = 2 * x;
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
+
+
+
+// to be more fast and more practical instead-of writte every letter just use
+// sysout CTRL + TAB;
